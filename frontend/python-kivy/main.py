34cc239bf8ef4e40fc5ffb2c64ab238b5ab72d7d
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-04-08 17:05:14 +0530

    changed

--- frontend/python-kivy/main.py
+++ frontend/python-kivy/main.py
@@ -1,14 +1,17 @@
 from re import MULTILINE
+from ssl import _PasswordType
 from tkinter import Label
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.widget import widget
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectPoperty
 
-class MyGrid(widget):
+class MyGrid(Widget):
     pass
+    
 
         
 class MyApp(App):
