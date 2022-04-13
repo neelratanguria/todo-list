3affb2dfa3f39df7b730d4ea50d1fd7911b2d383
Neel Ratan Guria <neelratan@live.com>
2022-04-13 05:43:21 +0530

    react app

---
+++ frontend/reactapp/src/GreetComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, Button, View, TextInput } from 'react-native';
+
+
+const GreetComponent = (props) => {
+    const onButtonPress = () => {
+        console.log("Hey sexy")
+    }
+    return (
+        <View>
+            <TextInput
+                style={{ padding: 10, borderWidth: 1, marginHorizontal: 20, marginVertical: 10 }}
+                placeholder={"Email"}
+            />
+            <TextInput
+                style={{ padding: 10, borderWidth: 1, marginHorizontal: 20, marginVertical: 10 }}
+                placeholder={"Password"}
+            />
+            <View style={{ margin: 20 }}>
+                <Button
+                    title={props.buttonName}
+                    onPress={onButtonPress}
+                    
+                />
+            </View>
+        </View>
+    )
+}
+
+
+export default GreetComponent
