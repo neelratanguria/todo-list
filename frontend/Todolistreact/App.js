a623f1b2dd1010e3df7a28fd34a4fe0e5982560f
Neel Ratan Guria <neelratan@live.com>
2022-04-15 20:13:15 +0530

    update

--- frontend/Todolistreact/App.js
+++ frontend/Todolistreact/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-
+import GreetComponent from './src/GreetComponent';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,6 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Image
 } from 'react-native';
 
 import {
@@ -39,10 +40,41 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        
+        <View style={{ alignItems: 'stretch', justifyContent: 'center', height: "100%" }}>
+        <Text style={{ textAlign: "center", fontSize: 40 }}>Todo List</Text>
+        <View style={{alignItems: "center", borderWidth: 2, marginHorizontal: 125}}>
+          <Image
+            style={styles.logo}
+            source={require('./assets/newproduct.png')}
+          />
+        </View>
+        <GreetComponent buttonName="Sign Up"
+        />
+      </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+  content: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%'
+  },
+  item: {},
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    margin: 10,
+    
+  },
+});
 
 export default App;
