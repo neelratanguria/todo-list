b92fa234204178a7c401ad1f6f8e0dc223772057
Neel Ratan Guria <neelratan@live.com>
2022-04-24 11:41:34 +0530

    react native sign up

--- frontend/Todolistreact/src/GreetComponent.js
+++ frontend/Todolistreact/src/GreetComponent.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, Image, Button, View, TextInput } from 'react-native';
+import axios from "axios";
 
+var mEmail
+const onEmailType = (text) => {
+    mEmail = text
+}
+
+var mPassword
+const onPasswordType = (text) => {
+    mPassword = text
+}
 
 const GreetComponent = (props) => {
+
+    const [maskPasswordState, setMaskState] = useState(true)
+    var maskPassword = false
+
     const onButtonPress = () => {
-        console.log("Hey sexy")
+        console.log("creating user")
+        axios.post("https://parseapi.back4app.com/users", {
+            "username": mEmail,
+            "password": mPassword
+        }, {
+            headers: {
+                'X-Parse-Application-Id': '',
+                'X-Parse-REST-API-Key': ''
+            }
+        }).then((res) => {
+            console.log(res.status)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const onPassTogglePress = () => {
+        console.log("view val toggled")
+        if (maskPasswordState) {
+            setMaskState(false)
+        } else {
+            setMaskState(true)
+        }
     }
     return (
         <View>
             <TextInput
                 style={{ padding: 10, borderWidth: 1, marginHorizontal: 20, marginVertical: 10 }}
                 placeholder={"Email"}
+                onChangeText={onEmailType}
             />
             <TextInput
                 style={{ padding: 10, borderWidth: 1, marginHorizontal: 20, marginVertical: 10 }}
                 placeholder={"Password"}
+                onChangeText={onPasswordType}
+                secureTextEntry={maskPasswordState}
             />
+            <View style={{ margin: 20 }}>
+                <Button
+                    title={"Toggle Password view"}
+                    onPress={onPassTogglePress}
+                />
+            </View>
             <View style={{ margin: 20 }}>
                 <Button
                     title={props.buttonName}
                     onPress={onButtonPress}
-                    
                 />
             </View>
         </View>
---
+++ frontend/Todolistreact/src/awesomeButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Button } from 'react-native-ui-kitten';
+export const AwesomeButton = () => (
+ <Button>BUTTON</Button>
+);
